501a2a1fc7474c8a504de1418d9b5781a6179bb4
Alansno <techoro1230@gmail.com>
2024-05-11 15:02:30 -0600

    cambios en archivos css

--- resources/views/sing-in.blade.php
+++ resources/views/sing-in.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('styles/sing-in.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('styles/sing-in.css') }}">
     <title>Iniciar sesión</title>
 </head>
 <body>
